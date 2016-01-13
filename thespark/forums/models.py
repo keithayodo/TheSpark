5be972bfe5ca4fd8a4416edb714297daf33ae924
Nigel Nindo <nigelnindo@gmail.com>
2016-01-13 05:10:30 +0300

    Added ForumReport and LastForumMessage models to forums app.

--- thespark/forums/models.py
+++ thespark/forums/models.py
@@ -38,7 +38,30 @@ class ForumMessage(models.Model):
     def __unicode__(self):
         return '{0} :{1}'.format(self.sender,self.message)
 
+class LastForumMessage(models.Model):
+    relation = models.OneToOneField(Forum,help_text="Last/latest message sent in forum.")
+    first_name = models.CharField(max_length=200,help_text="First name of last message sender.")
+    last_name = models.CharField(max_length=200,help_text="Last name of last message sender.")
+    mesasge = models.CharField(max_length=1000,help_text="Last message sent in forum.")
+    created_at = models.DateTimeField(help_text="Time when last message was sent in forum.")
+
+    def __unicode__(self):
+        return '{0} :{1}'.format(self.relation,self.message)
+
 class ForumRequest(models.Model):
     relation = models.ForeignKey(SparkUser,help_text="Spark user requesting a new forum.")
+    forum_name = models.CharField(max_length=50,help_text="Proposed forum name.")
     message = models.CharField(max_length=1000,help_text="Message content.")
     created_at = models.DateTimeField(editable=False,auto_now_add=True,help_text="Date and time of forum request submission.")
+
+    def __unicode__(self):
+        return '{0} : {1}'.format(self.relation,self.forum_name)
+
+class ForumReport(models.Model):
+    relation = models.ForeignKey(SparkUser,help_text="Spark user who is reporting a forum.")
+    forum_relation = models.ForeignKey(Forum,help_text="Forum being reported.")
+    message = models.CharField(max_length=1000,help_text="Message of reporter.")
+    created_at = models.DateTimeField(editable=False,auto_now_add=True,help_text="Date and time when the forum was reported.")
+
+    def __unicode__(self):
+        return '{0} => {1}'.format(self.relation, self.forum_relation.title)
--- thespark/forums/services.py
+++ thespark/forums/services.py
@@ -3,6 +3,10 @@ from rest_framework import (
     exceptions
 )
 
+from user.services import (
+    AllUserSerializer,
+)
+
 from users.models import (
     SparkUser,
     CounsellorUser,
@@ -14,6 +18,24 @@ from .models import (
     ForumMessage,
     ForumRequest,
 )
+
+class ForumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Forum
+        fields = ('pk','title','description','created_at','updated_at',)
+
+class ForumMemberSerializer(serializers.ModelSerializer):
+    forum_relation = ForumSerializer()
+    user_relation = AllUserSerializer()
+    class Meta:
+        model = ForumMember
+        fields = ('pk','forum_relation','user_relation',) #won't probably need to use this pk
+
+class ForumRequestSerializer(serializers.ModelSerializer):
+    relation = SparkUser()
+    class Meta:
+        model = ForumRequest
+        fields = ('relation','message','created_at',)
 
 class ForumService:
     pass
